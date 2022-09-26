9f3d426da2f6dbbb17c8f899d691564416aa64c2
moad Zizi <moad.zizi@hotmail.com>
2022-09-26 15:48:31 +0200

    Add Blog
    this is for adding a blog into the systeme

--- src/pages/Blogs/Blogs.jsx
+++ src/pages/Blogs/Blogs.jsx
@@ -1,47 +1,62 @@
 import './Blogs.css'
 import React, { useState, useEffect } from 'react'
-import { Grid } from '@mui/material'
+import { Grid, TextField } from '@mui/material'
 import Post from '../../components/Post'
 import Data from '../../DATA/Data.json'
 import Modal from '@mui/material/Modal';
-import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 
 export default function Blogs() {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState(Data.posts);
   const [open, setOpen] = useState(false);
+  const [title, setTitle] = useState('');
+  const [blog, setBlog] = useState('');
+
+  const handleChangeTitle = (event) => {
+    setTitle(event.target.value);
+  };
+  const handleChangeBlog = (event) => {
+    setBlog(event.target.value);
+  };
+
+  const handleDate = () => {
+    const date = new Date();
+    const [month, day, year] = [date.getMonth() + 1, date.getDate(), date.getFullYear()];
+    return (day + '/' + month + '/' + year)
+  }
 
   const style = {
     position: 'absolute',
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    width: 400,
     bgcolor: 'background.paper',
     border: '2px solid #000',
     boxShadow: 24,
     pt: 2,
     px: 4,
     pb: 3,
+    justify: 'center'
   };
 
-  useEffect(() => {
-    setData(Data.posts)
-  }, []);
-
   const DeleteHandler = (id) => {
-    //alert(id);
+
   }
+
   const handleOpen = () => {
     setOpen(true);
   };
   const handleClose = () => {
     setOpen(false);
   };
+  const submitData = () => {
+    let newData = [...data, { title: title, id: 55, body: blog, DatePost: handleDate() }]
+    setData(newData)
+    setOpen(false);
+  }
 
   return (
     <div className='Blogs'>
-
       <Grid container spacing={2}>
         {
           data.map((blog) => {
@@ -52,15 +67,42 @@ export default function Blogs() {
           })
         }
       </Grid>
-      <Button onClick={handleOpen}>Open modal</Button>
+      <div className="ContainerFab">
+        <button onClick={handleOpen} className="FAB"> Add Article </button>
+
+      </div>
       <Modal
         open={open}
         onClose={handleClose}
         aria-labelledby="parent-modal-title"
         aria-describedby="parent-modal-description"
       >
-        <Box sx={{...style, width: 400 }}>
-          <h2> Add new Article</h2>
+        <Box sx={{ ...style, width: "50%" }}>
+
+          <h2 className="modalTitle"> Add new Article</h2>
+          <TextField
+            id="outlined-basic"
+            label="Blog Title"
+            variant="outlined"
+            className='modalTitleInput'
+            sx={{ margin: '20px' }}
+            placeholder='Awsome Title'
+            onChange={handleChangeTitle}
+          />
+          <br />
+          <TextField
+            id="outlined-basic"
+            label="Write your blog here"
+            variant="outlined"
+            className='modalBlogInput'
+            multiline
+            placeholder='Write something intresting, be creative'
+            fullWidth
+            rows={7}
+            sx={{ margin: '20px' }}
+            onChange={handleChangeBlog}
+          />
+          <button onClick={submitData} className="submitButton"> Submit </button>
         </Box>
       </Modal>
     </div>
